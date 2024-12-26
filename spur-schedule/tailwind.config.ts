8dcfe1d7bdb6823a792be93c440db8d6f0fa5fb6
Stephen Desk <wonhs91@gmail.com>
2024-12-26 18:06:58 -0500

    Initial next typescript

--- spur-schedule/tailwind.config.ts
+++ spur-schedule/tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,4 +15,4 @@ export default {
     },
   },
   plugins: [],
-};
+} satisfies Config;
